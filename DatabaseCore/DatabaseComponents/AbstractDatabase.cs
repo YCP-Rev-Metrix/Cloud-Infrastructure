5b4f84dc92f34811ed43c0d91227fb89bf76267f
Robert Wood <L.robertwood@gmail.com>
2024-03-25 21:26:50 -0400

    Added commented out methods for API security, need to get functionality working then can start subbing into methods

--- DatabaseCore/DatabaseComponents/AbstractDatabase.cs
+++ DatabaseCore/DatabaseComponents/AbstractDatabase.cs
@@ -23,7 +23,35 @@ public abstract class AbstractDatabase
                 ? $"Server=143.110.146.58,1433;User Id=SA;Password=BigPass@Word!;TrustServerCertificate=True;"
                 : $"Data Source=localhost;Integrated Security=True;TrustServerCertificate=True;";
 
+        //LogWriter.LogInfo($"Server=143.110.146.58,1433;User Id=SA;Password=BigPass@Word!;TrustServerCertificate=True;");
+
+
+
+
+
+
+
+
+        /*********************************************************************************************************
+         * THIS WILL BE USED FOR SECURITY, NEED TO FIX PROCESSING OF ENV VARAIBLES LOCALLY BEFORE COMMENTING OUT *
+         *********************************************************************************************************/
+
+        /*// Get DOCKERIZED and Connection String environment variables
+        string? DockerizedEnviron = Environment.GetEnvironmentVariable("DOCKERIZED");
+        string? ServerConnectionString = "" + Environment.GetEnvironmentVariable("SERVER_CONNECTION_STRING");
+        string? LocalConnectionString = "" + Environment.GetEnvironmentVariable("LOCAL_CONNECTION_STRING");
+
+        ConnectionString = DockerizedEnviron == null
+            ? $"{ServerConnectionString}" // Force cloud connection string since local development does not work at the moment
+            // LocalConnectionString // uncomment this local connection string once local dev works again 
+            : DockerizedEnviron == "Dockerized"
+                ? $"{ServerConnectionString}"
+                : $"{LocalConnectionString}";
+
         LogWriter.LogInfo($"DB Connection: {ConnectionString}");
+        LogWriter.LogInfo($"Server String: {ServerConnectionString}");
+        LogWriter.LogInfo($"Local String: {LocalConnectionString}");*/
+
 
         DatabaseName = databaseName;
         Initialize();
@@ -36,9 +64,8 @@ public abstract class AbstractDatabase
         serverConnection = new ServerConnection("localhost");
         Server = new Server(serverConnection);
         */
+
         Server = new Server(new ServerConnection(new SqlConnection(ConnectionString)));
         database = Server.Databases[DatabaseName];
-
     }
-
 }
